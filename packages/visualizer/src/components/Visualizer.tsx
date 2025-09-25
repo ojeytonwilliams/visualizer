4d64ff1e055fef459e19c5a7c220dc70c2557a55
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-09-25 11:06:51 +0200

    feat: create toy frontend

---
+++ packages/visualizer/src/components/Visualizer.tsx
@@ -0,0 +1,14 @@
+interface VisualizerProps {}
+
+export default function Visualizer({}: VisualizerProps) {
+  return (
+    <div style={{
+      fontSize: '3rem',
+      fontWeight: 'bold',
+      color: '#333',
+      textAlign: 'center'
+    }}>
+      Visualiser
+    </div>
+  );
+}
