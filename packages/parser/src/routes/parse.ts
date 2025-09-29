7cc6fd739ad91ab4aa6caeffa9369d65a794ae1c
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-09-29 16:14:56 +0200

    debug: time parsing + scanning

--- packages/parser/src/routes/parse.ts
+++ packages/parser/src/routes/parse.ts
@@ -7,8 +7,12 @@ export async function parser(fastify: FastifyInstance) {
   fastify.post('/dependency-map', async req => {
     const folderPath = (req.body as { folderPath: string }).folderPath;
     req.log.info('Received request to parse folder:' + folderPath);
+    console.time('scanning');
     const files = await scanDirectory(folderPath);
+    console.timeEnd('scanning');
+    console.time('parsing');
     const dependencyMap = await parse(files);
+    console.timeEnd('parsing');
     return { success: true, folderPath, dependencyMap };
   });
 }
